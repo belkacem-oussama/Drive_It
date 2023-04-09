e086d06ee733e6acd0db65bca0a613995f169383
Belkacem Oussama <oussama.blkc@gmail.com>
2023-04-09 14:59:12 +0200

    complete header

--- app/views/header.tpl.php
+++ app/views/header.tpl.php
@@ -7,3 +7,34 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   </head>
   <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Drive it !</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Commandes
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">en cours</a></li>
+              <li><a class="dropdown-item" href="#">à venir</a></li>
+              <li><a class="dropdown-item" href="#">clôturées</a></li>
+            </ul>
+          </li>
+          <a class="nav-link" aria-current="page" href="#">Réparation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Flotte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Concessions</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
