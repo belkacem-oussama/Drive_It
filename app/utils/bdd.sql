142ac70f7bf009b4c2b931e76f31e8f89f57e27a
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-09-01 11:44:04 +0200

    maj sql sscript

--- app/utils/bdd.sql
+++ app/utils/bdd.sql
@@ -29,7 +29,7 @@ CREATE TABLE `dealer` (
   `brand` varchar(50) NOT NULL,
   `contact_name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `phone` int(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
   `mail` varchar(255) NOT NULL,
   `CarsId` int(11) NOT NULL,
   PRIMARY KEY (`DealerId`),
@@ -38,9 +38,9 @@ CREATE TABLE `dealer` (
 
 INSERT INTO `dealer` (`brand`, `contact_name`, `address`, `phone`, `mail`, `CarsId`)
 VALUES
-('Peugeot', 'Jean Dupont', '2 Rue de la Paix, 75001 Paris', 1234567890, 'jeandupont@peugeot.fr', 2),
-('Renault', 'Sophie Martin', '15 Avenue des Champs-Élysées, 75008 Paris', 9876543210, 'sophiemartin@renault.fr', 3),
-('Citroën', 'Pierre Dubois', '50 Avenue Montaigne, 75008 Paris', 1234567890, 'pierredubois@citroen.fr', 1);
+('Peugeot', 'Jean Dupont', '2 Rue de la Paix, 75001 Paris', '0123456789', 'jeandupont@peugeot.fr', 2),
+('Renault', 'Sophie Martin', '15 Avenue des Champs-Élysées, 75008 Paris', '0987654321', 'sophiemartin@renault.fr', 3),
+('Citroën', 'Pierre Dubois', '50 Avenue Montaigne, 75008 Paris', '0123456789', 'pierredubois@citroen.fr', 1);
 
 DROP TABLE IF EXISTS `driver`;
 CREATE TABLE `driver` (
@@ -49,22 +49,23 @@ CREATE TABLE `driver` (
   `lastname` varchar(255) NOT NULL,
   `age` varchar(5) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `phone` int(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
   `mail` varchar(255) NOT NULL,
-  `licence_number` int(100) NOT NULL,
+  `licence_number` varchar(11) NOT NULL,
   PRIMARY KEY (`DriverId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `driver` (`firstname`, `lastname`, `age`, `address`, `phone`, `mail`, `licence_number`)
 VALUES
-('Jean', 'Dupont', '25', '14 rue des Lilas, 75010 Paris', 654321987, 'jean.dupont@mail.com', 123456),
-('Marie', 'Martin', '32', '26 avenue des Champs-Élysées, 75008 Paris', 678901234, 'marie.martin@mail.com', 234567),
-('Pierre', 'Durand', '45', '2 rue de la Paix, 75001 Paris', 687654321, 'pierre.durand@mail.com', 345678);
+('Jean', 'Dupont', '25', '14 rue des Lilas, 75010 Paris', '0654321987', 'jean.dupont@mail.com', '123456'),
+('Marie', 'Martin', '32', '26 avenue des Champs-Élysées, 75008 Paris', '0678901234', 'marie.martin@mail.com', '234567'),
+('Pierre', 'Durand', '45', '2 rue de la Paix, 75001 Paris', '0687654321', 'pierre.durand@mail.com', '345678');
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE `order` (
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
   `OrderId` int(11) NOT NULL AUTO_INCREMENT,
-  `order_date` date NOT NULL DEFAULT current_timestamp(),
+  `order_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `rent_start` date NOT NULL,
   `rent_end` date NOT NULL,
   `km_start` int(10) NOT NULL,
@@ -74,12 +75,12 @@ CREATE TABLE `order` (
   `DriverId` int(11) NOT NULL,
   `CarsId` int(11) NOT NULL,
   PRIMARY KEY (`OrderId`),
-  FOREIGN KEY (`DriverId`) REFERENCES driver(`DriverId`),
-  FOREIGN KEY (`CarsId`) REFERENCES cars(`CarsId`)
+  FOREIGN KEY (`DriverId`) REFERENCES `driver`(`DriverId`),
+  FOREIGN KEY (`CarsId`) REFERENCES `cars`(`CarsId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `order` (`rent_start`, `rent_end`, `km_start`, `km_end`, `comments`,`status`, `DriverId`, `CarsId`)
+INSERT INTO `orders` (`rent_start`, `rent_end`, `km_start`, `km_end`, `comments`, `status`, `DriverId`, `CarsId`)
 VALUES
-('2023-04-10', '2023-04-12', 100, 200, 'Commentaires sur la première commande',0, 1, 1),
-('2023-04-11', '2023-04-13', 200, 300, 'Commentaires sur la deuxième commande',1, 2, 2),
-('2023-04-12', '2023-04-14', 300, 400, 'Commentaires sur la troisième commande',2, 3, 3);
+('2023-04-10', '2023-04-12', 100, 200, 'Commentaires sur la première commande', 0, 1, 1),
+('2023-04-11', '2023-04-13', 200, 300, 'Commentaires sur la deuxième commande', 1, 2, 2),
+('2023-04-12', '2023-04-14', 300, 400, 'Commentaires sur la troisième commande', 2, 3, 3);
